e710da9c3a46c7daf343819648e77e4813b91a89
Dmytro-1506 <kyunlik@gmail.com>
2022-12-29 15:30:27 +0100

    it works better now

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import axios from 'axios';
-import Notiflix from "notiflix";
-
-import { request } from "./js_files/fetch_pixabay";
+import MyNotifications from "./js_files/notifications";
 
 import NewsApiServer from "./js_files/newsApiServer";
 
+const myNotifications = new MyNotifications;
 const newApiServer = new NewsApiServer();
 
 const refs = {
@@ -17,7 +15,66 @@ const refs = {
     loadMoreBtn: document.querySelector('.load-more'),
 }
 
+let pageNumber = 1;
+let totalHits = null;
+let numberOfImages = 0;
+
 refs.form.addEventListener('submit', (event) => {
     event.preventDefault();
-    newApiServer.requestName = refs.input.value;
+    refs.loadMoreBtn.classList.add('visually-hidden');
+    refs.gallery.innerHTML = '';
+    pageNumber = 1;
+    numberOfImages = 0;
+    newApiServer.requestName = refs.input.value.trim();
+
+    if (newApiServer.requestName) {
+        getMyPhotos();
+    }
 })
+
+async function getMyPhotos() {
+    const myPhotos = await newApiServer.fetchImages(pageNumber);
+
+    totalHits = myPhotos.data.totalHits;
+    if (!totalHits) {
+        myNotifications.notFound();
+    } else if (numberOfImages === 0) {
+        myNotifications.quantityOfImages(totalHits);
+        refs.loadMoreBtn.classList.remove('visually-hidden');
+    }
+
+    const parametersArrey = [];
+    myPhotos.data.hits.map(el => {
+        parametersArrey.push(
+            {
+                webformatURL: el.webformatURL,
+                largeImageURL: el.largeImageURL,
+                tags: el.tags,
+                likes: el.likes,
+                views: el.views,
+                comments: el.comments,
+                downloads: el.downloads
+            }
+        )
+    });
+    const photosList = [];
+    parametersArrey.map(photo => {
+        photosList.push(`<div class="photo-card gallery__item"><a class="gallery__link" href=${photo.largeImageURL}><img class="gallery__image" src=${photo.webformatURL} alt=${photo.tags} loading="lazy" /></a><div class="info"><p class="info-item"><b>Views: ${photo.views} | Comments: ${photo.comments}</b></p><p class="info-item"><b>Likes: ${photo.likes} | Downloads: ${photo.downloads}</b></p></div></div>`);
+    });
+    refs.gallery.insertAdjacentHTML("beforeend", photosList.join(''));
+    numberOfImages = refs.gallery.children.length;
+    if (totalHits === numberOfImages && numberOfImages > 0) {
+        myNotifications.endOfSerchResults();
+        refs.loadMoreBtn.classList.add('visually-hidden');
+    }
+    let gallery = new SimpleLightbox('.gallery a');
+}
+
+refs.gallery.addEventListener('click', (event) => {
+    event.preventDefault();
+});
+
+refs.loadMoreBtn.addEventListener('click', () => {
+    pageNumber += 1;
+    getMyPhotos();
+});
--- src/js_files/newsApiServer.js
+++ src/js_files/newsApiServer.js
@@ -1,10 +1,18 @@
+import axios from 'axios';
 export default class NewsApiServer {
     constructor(){
-        this.requestName = '';
+        let requestName = '';
     }
 
-    fetchImages() {
+    async fetchImages(pageNumber) {
         const KEY = '32403281-07d99c56a2826923173cf204d';
+
+        try {
+            const request = await axios.get(`https://pixabay.com/api/?key=${KEY}&q=${this.requestName}&image_type=photo&orientation=horizontal&safesearch=true&page=${pageNumber}&per_page=40`);
+            return request;
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     get request() {
